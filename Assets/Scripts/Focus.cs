5fa32d95f54714a96a10b3d7bba784123f6341a0
Samuel Proulx <proulxsamuel@gmail.com>
2019-01-26 11:53:20 -0500

    Implement alternative spotlight control script

---
+++ Assets/Scripts/Focus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Focus : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Light light;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    void FixedUpdate()
+    {
+        var difference = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(difference);
+        var distance = difference.magnitude;
+        light.range = distance * 2;
+        light.spotAngle = Mathf.Max(-0.05f * distance + 10.0f, 0.01f);
+    }
+}
