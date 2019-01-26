08192670ea023affe2b9aada8092425bebd25a97
Samuel Proulx <proulxsamuel@gmail.com>
2019-01-26 12:20:29 -0500

    Implement initial panic system with light detection

---
+++ Assets/Scripts/Panic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panic : MonoBehaviour
+{
+    [SerializeField] Light light;
+    [SerializeField] float level = 0;
+    [SerializeField] float max = 100;
+    [SerializeField] float regenRate = 1;
+
+    void FixedUpdate()
+    {
+        if (InSpotLight())
+        {
+            level += regenRate * Time.deltaTime;
+            level = Mathf.Min(level, max);
+        }
+    }
+
+    bool InSpotLight()
+    {
+        var line = transform.position - light.transform.position;
+        var angle = Vector3.Angle(line, light.transform.forward);
+        return angle < light.spotAngle;
+    }
+}
