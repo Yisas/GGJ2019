3b002c866494b55da680f68bee8dd3663be43466
Jesus Imery <yisas_0929@hotmail.com>
2019-01-26 21:27:15 -0500

    #8 Game mood tracks lacking transition

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip normalMood;
+    public AudioClip panickedMood;
+    public AudioClip normalToPanicTransition;
+    public AudioClip panicToNormalTransition;
+
+    public float moodTransitionDuration;
+
+    private enum Transitioning { No, ToNormal, ToPanic }
+    private Transitioning transitioning;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transitioning == Transitioning.ToNormal && !audioSource.isPlaying)
+        {
+            Normal();
+        }
+
+        if (transitioning == Transitioning.ToPanic && !audioSource.isPlaying)
+        {
+            Panic();
+        }
+    }
+
+    public void TransitionToPanic()
+    {
+        audioSource.clip = normalToPanicTransition;
+        audioSource.Play();
+        transitioning = Transitioning.ToPanic;
+    }
+
+    public void TransitionToNormal()
+    {
+        audioSource.clip = panicToNormalTransition;
+        audioSource.Play();
+        transitioning = Transitioning.ToNormal;
+    }
+
+    public void Panic()
+    {
+        audioSource.clip = panickedMood;
+        audioSource.Play();
+        transitioning = Transitioning.No;
+    }
+
+    public void Normal()
+    {
+        audioSource.clip = normalMood;
+        audioSource.Play();
+        transitioning = Transitioning.No;
+    }
+}
