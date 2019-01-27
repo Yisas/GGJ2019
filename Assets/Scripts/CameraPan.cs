731ddbbb06ead5a6dfb09de2dd6d3d4d837a5d71
Samuel Proulx <proulxsamuel@gmail.com>
2019-01-27 10:45:55 -0500

    Implement camera panning for end of level

---
+++ Assets/Scripts/CameraPan.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float startDistance;
+    [SerializeField] float transitionDistance = 1;
+
+    Quaternion initialRotation;
+
+    void Start()
+    {
+        initialRotation = transform.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        var vector = target.transform.position - transform.position;
+        var distance = vector.magnitude;
+        if (distance < startDistance)
+        {
+            transform.rotation = Quaternion.Lerp(
+                initialRotation,
+                Quaternion.LookRotation(vector),
+                (startDistance - distance) / transitionDistance
+            );
+        }
+    }
+}
