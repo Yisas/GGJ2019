d7330f6234a1ccdfa88e03ce76fe24de5cd3afb6
Samuel Proulx <proulxsamuel@gmail.com>
2019-01-26 22:17:41 -0500

    Add the wind direction arrow to the HUD

---
+++ Assets/Scripts/WindArrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArrow : MonoBehaviour
+{
+    public float smoothness = 1;
+
+    Wind wind;
+    Vector3 initialScale;
+
+    void Start()
+    {
+        var found = GameObject.Find("Wind");
+        if (found)
+        {
+            wind = found.GetComponent<Wind>();
+        }
+        initialScale = transform.localScale;
+    }
+
+    void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation,
+            Quaternion.Euler(0.0f, 0.0f, -wind.direction + 90),
+            Time.deltaTime / smoothness
+        );
+        transform.localScale = Vector3.Lerp(
+            transform.localScale,
+            wind.speed / wind.maxSpeed * initialScale,
+            Time.deltaTime / smoothness
+        );
+    }
+}
