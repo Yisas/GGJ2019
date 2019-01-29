3750d0a4e97e2b517be88a86b69f05b86956c8e3
Jesus Imery <yisas_0929@hotmail.com>
2019-01-29 03:43:14 -0500

    #15 Game restart after ending

--- Assets/Scripts/UI/FinalLevelCanvas.cs
+++ Assets/Scripts/UI/FinalLevelCanvas.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FinalLevelCanvas : MonoBehaviour
 {
     public Image blackoutImage;
     public float fadeSpeed;
+    public float timeToRestart;
+
+    private float timer;
+    private bool restarting = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = timeToRestart;
         StartCoroutine("FadeInCoroutine");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if(timer <= 0 && !restarting)
+        {
+            Restart();
+            
+        }
     }
 
     IEnumerator FadeInCoroutine()
@@ -35,5 +46,30 @@ public class FinalLevelCanvas : MonoBehaviour
         c = blackoutImage.color;
         c.a = 0;
         blackoutImage.color = c;
+    }
+
+    IEnumerator FadeOutCoroutine()
+    {
+        Color c;
+
+        for (float f = 0f; f <= 1.0f; f += fadeSpeed)
+        {
+            c = blackoutImage.color;
+            c.a = f;
+            blackoutImage.color = c;
+            yield return null;
+        }
+
+        c = blackoutImage.color;
+        c.a = 1;
+        blackoutImage.color = c;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Restart()
+    {
+        restarting = true;
+        Destroy(FindObjectOfType<SoundManager>().gameObject);
+        StartCoroutine("FadeOutCoroutine");
     }
 }
